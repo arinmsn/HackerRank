5ceef6692ebe4726aa814836b4f9d05e63014d37
arinmsn <arinmsn@gmail.com>
2019-09-13 20:53:04 -0700

    Need to run tests. App works.

---
+++ RepeatedString/RepeatedString.js
@@ -0,0 +1,12 @@
+function repeatedString(s, n) {
+  let x = Math.floor(n / s.length);
+  let count = (s.split("a").length - 1) * x;
+  let rem = n % s.length;
+
+  for (var i = 0; i < rem; i++) {
+    if (s.charAt(i) === "a") {
+      count++;
+    }
+  }
+  return count;
+}
