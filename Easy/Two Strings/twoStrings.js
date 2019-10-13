efe686fb7c3db1074e2b657f0a75f12e24268f96
arinmsn <arinmsn@gmail.com>
2019-10-12 21:01:18 -0700

    Two strings - solved

---
+++ Easy/Two Strings/twoStrings.js
@@ -0,0 +1,12 @@
+function twoStrings(s1, s2) {
+  let result = "NO";
+  const s1len = s1.length;
+  const s2len = s2.length;
+  if (s1len >= 1 && s1len <= 100000 && s2len >= 1 && s2len <= 100000) {
+    result =
+      s1.split("").filter((el, key) => s2.indexOf(el) > -1).length > 0
+        ? "YES"
+        : "NO";
+  }
+  return result;
+}
